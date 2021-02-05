9c35d252bff23c8d7587f9131355542df282959c
henok <henokwel@outlook.com>
2021-02-06 00:11:35 +0100

    more fn

--- scripts/function.sh
+++ scripts/function.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Gretting function
 
 function gretting(){
@@ -6,14 +7,22 @@ function gretting(){
     read -p  "Hello, My name is  $name. Whats yours " yourname
     echo "Welcome $yourname .Nice to have you here."
 }
-echo $(gretting)
-
-
+# echo $(gretting)
 
 function sayHello(){
-
+    # -p stands for prompt
     read -p "Whats your name ?" name
     echo "Hello $name"
 }
+# echo $(sayHello)
 
-echo $(sayHello)
+# Older version of calling function
+# echo `sayHello`
+
+#Using parameter to get user input => ./file.sh last
+last=$1
+
+read -p "Please enter your name ? " name
+echo "Hello $name $last"
+
+
