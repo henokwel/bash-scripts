ce8dc763a37bdadd87e0bacc13492bfa3795b4e6
henokwel <hen.ok@live.com>
2021-02-06 18:55:50 +0100

    more loops

--- scripts/iterations.sh
+++ scripts/iterations.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 # While
+count=0
+
+# while [ $count -le 5 ]
+# do 
+# echo "Welcome x $count "
+# count=$(($count + 1))
+# done
+# echo $count
 
 
+# While with if 
+
+echo "------"
+while [ $count -le 5 ];
+ do
+if [  $count == 2 ];
+then 
+echo "yeahhh "
+fi
+
+echo $count
+count=$((count+1))
+done
 
 #For 
-
-
-
