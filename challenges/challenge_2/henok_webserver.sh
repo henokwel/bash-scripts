9a87e3de9b673a6502474e55ce8dfd1c0564ea1c
henokwel <hen.ok@live.com>
2021-02-07 19:46:00 +0100

    second challenge added

---
+++ challenges/challenge_2/henok_webserver.sh
@@ -0,0 +1,12 @@
+
+# Update packages 
+sudo apt-get update -y
+
+# Install the nginx package
+# the -y is for Yes. This avoid the prompt from asking you.
+sudo apt-get install nginx -y
+
+# visit the localhost web server
+curl localhost
+
+
